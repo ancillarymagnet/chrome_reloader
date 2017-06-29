65e47d06e7d8b7775c97c6042905c7cb36f52a9a
Noah Norman <doctorhandshake@gmail.com>
2017-06-28 20:22:35 -0400

    args

--- reloader.py
+++ reloader.py
@@ -1,18 +1,22 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import sys
 
 RELOAD_INTERVAL = 5 * 60
-URL = 'http://historyofthegif.com'
+
+if 
+
+url = sys.argv[1]
 
 options = Options()
 options.add_argument("--kiosk")
-options.add_argument("--app=" + URL)
+options.add_argument("--app=" + url)
 options.add_argument("disable-infobars")
 
 while(True):
 	driver = webdriver.Chrome(chrome_options=options)
-	driver.get(URL)
+	driver.get(url)
 	time.sleep(RELOAD_INTERVAL)
 	# driver.close()
 	driver.quit()
